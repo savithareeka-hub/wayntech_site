996ff12002d84c1a326dfc989d85b56ba5a8def3
savithareeka-hub <savithareeka@gmail.com>
2026-04-03 19:21:54 +0530

    Add files via upload

---
+++ apps/web/src/data/categories.js
@@ -0,0 +1,29 @@
+// src/data/categories.js
+
+export const categories = [
+  {
+    name: "ID Cards",
+    image: "/images/cat1.png",
+    description: "Durable and professional ID cards for offices and schools."
+  },
+  {
+    name: "Custom Keychains",
+    image: "/images/cat1.png",
+    description: "Custom keychains with names, photos, or logos."
+  },
+  {
+    name: "Photo Gifts",
+    image: "/images/cat1.png",
+    description: "Personalized gifts like mugs, frames, and prints."
+  },
+  {
+    name: "Custom Stamps & Ink",
+    image: "/images/cat1.png",
+    description: "High-quality stamps for office and business use."
+  },
+  {
+    name: "Visiting Cards",
+    image: "/images/cat1.png",
+    description: "Premium business cards with modern design."
+  }
+];
--- apps/web/src/data/products.js
+++ apps/web/src/data/products.js
@@ -4,27 +4,47 @@ export const products = [
     name: "Standard ID Cards",
     price: 50,
     category: "ID Cards",
-    image: "/images/id1.jpg"
+    image: "/images/tag 1.png",
+    description: "Durable PVC ID cards for offices, schools, and events."
   },
   {
     id: 2,
     name: "Premium ID Cards",
     price: 80,
     category: "ID Cards",
-    image: "/images/id2.jpg"
+    image: "/images/tag 2.png",
+    description: "Premium glossy ID cards with high durability and professional finish."
   },
   {
     id: 3,
-    name: "Customised Lanyards",
+    name: "Custom Keychain",
     price: 30,
-    category: "Lanyards",
-    image: "/images/lanyard.jpg"
+    category: "Custom Keychains",
+    image: "/images/tag 2.png",
+    description: "Personalized keychains with names, photos, or logos."
   },
   {
     id: 4,
-    name: "Personalised T-Shirts",
+    name: "Photo Mug",
     price: 250,
-    category: "T-Shirts",
-    image: "/images/tshirt.jpg"
+    category: "Photo Gifts",
+    image: "/images/tag 2.png",
+    description: "Custom printed mugs perfect for gifts and branding."
+  },
+  {
+    id: 5,
+    name: "Stamp",
+    price: 250,
+    category: "Custom Stamps & Ink",
+    image: "/images/tag 2.png",
+    description: "Custom printed mugs perfect for gifts and branding."
+  },
+  {
+    id: 6,
+    name: "Stamp",
+    price: 250,
+    category: "Visiting Cards",
+    image: "/images/tag 2.png",
+    description: "Custom printed mugs perfect for gifts and branding."
   }
 ];
